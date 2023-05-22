4ec8ad93f00c0b0c0a68bc1e2932207d88f8f163
이진실 <79292184+ljsil1204@users.noreply.github.com>
2023-05-22 16:37:06 +0900

    express 기본 라우팅

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,28 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port: number = 8000;
 
+//라우팅
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
+app.post("/", (req: Request, res: Response) => {
+  res.send("Got a Post request");
+});
+
+app.get("/user", (req: Request, res: Response) => {
+  res.send("user!");
+});
+
+app.put("/user", (req: Request, res: Response) => {
+  res.send("Got a put request at /user");
+});
+
+app.delete("/user", (req: Request, res: Response) => {
+  res.send("Got a delete request at /user");
+});
+
+// 서버 대기 상태
 app.listen(port, () => {
   console.log(`listening http://localhost:${port}`);
 });
