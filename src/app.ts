c67b6923344e608778beeb42b115a5c7d550f603
이진실 <79292184+ljsil1204@users.noreply.github.com>
2023-05-22 16:31:33 +0900

    타입스크립트로 Hellow world 코드 적용해보기
    - "esModuleInterop": true 옵션 추가 => import express from "express" 에러 해결

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("hello world!!");
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port: number = 8000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`listening http://localhost:${port}`);
+});
